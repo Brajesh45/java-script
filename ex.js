8c8887bf185723a4dbe612b4f7d1b7d2c74d2c1e
Brajesh45 <brajeshsingh00777@gmail.com>
2024-09-26 22:50:44 +0530

    Add files via upload

---
+++ ex.js
@@ -0,0 +1,9 @@
+// A simple JavaScript program to print a message
+console.log("Hello, world!");
+
+// Simple math operation
+let a = 5;
+let b = 3;
+let sum = a + b;
+
+console.log("The sum of " + a + " and " + b + " is: " + sum);
